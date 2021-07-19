6c76fdc7120472a177e03e1806cdbf44ab146ee0
Quang Trương <quangklynk@gmail.com>
2021-07-19 23:28:37 +0700

    ok login chưa css

---
+++ src/components/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <form action="/action_page.php" @submit.prevent="handleSubmit()">
+      <div class="form-group">
+        <label for="email">Username: </label>
+
+        <input
+          type="email"
+          class="form-control"
+          placeholder="Enter Username"
+          v-model="username"
+        />
+      </div>
+      <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Enter password"
+          v-model="password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  computed: {},
+  methods: {
+    handleSubmit() {
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
+      console.log(data);
+      axios
+        .post("login", data)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+/* @import "./assets/scss/login.scss"; */
+label {
+  color: red;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import  Router  from 'vue-router'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {path: '/', component: Home},
+        {path: '/login', component: Login}
+    ]
+})
