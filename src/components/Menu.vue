96a56813d198dec37966f6a371a3586ee3a967a8
Võ Ngọc Tỷ <apple@Apples-MacBook-Pro.local>
2021-07-25 16:11:52 +0700

    menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,41 +1,125 @@
 <template>
-    <div>
-        <h2>Menu</h2>
-    </div>
+  <el-container style="height: 100vh">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-col :span="12">
+        <h5>Default colors</h5>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>Navigator One</span>
+            </template>
+            <el-menu-item-group title="Group One">
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item one</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">item four</template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span>Navigator Three</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span>Navigator Four</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>View</el-dropdown-item>
+            <el-dropdown-item>Add</el-dropdown-item>
+            <el-dropdown-item>Delete</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>Tom</span>
+      </el-header>
+
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="Date" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="Name" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="Address"> </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-    data() {
-        return {
-
-        }
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "Tom",
+      address: "No. 189, Grove St, Los Angeles",
+    };
+    return {
+      tableData: Array(20).fill(item),
+    };
+  },
+  // mounted() {
+  //     this.checkLoggedIn();
+  // },
+  created() {
+    this.checkLoggedIn();
+  },
+  methods: {
+    checkLoggedIn: function () {
+      let token = window.localStorage.getItem("token");
+      if (token == null) {
+        this.$router.push("/login");
+      }
+      axios
+        .get("user")
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(() => {
+          this.$router.push("/login");
+        });
     },
-    // mounted() {
-    //     this.checkLoggedIn();
-    // },
-    created() {
-        this.checkLoggedIn();
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
     },
-    methods: {
-        checkLoggedIn: function() {
-            let token = window.localStorage.getItem('token');
-            if (token == null) {
-                this.$router.push('/login');
-            }
-            axios.get('user')
-            .then(function(response) {
-                console.log(response);
-            })
-            .catch(() => {
-                this.$router.push('/login');
-            })
-        }
-    }
-}
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
 
 <style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
 
+.el-aside {
+  color: #333;
+}
 </style>
