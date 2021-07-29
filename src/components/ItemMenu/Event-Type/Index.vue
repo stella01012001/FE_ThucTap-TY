bd6ecddcdd9510761b73fbcc7da3f8758f0ecf6d
Quang Trương <quangklynk@gmail.com>
2021-07-29 23:10:41 +0700

    fix

--- src/components/ItemMenu/Event-Type/Index.vue
+++ src/components/ItemMenu/Event-Type/Index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <el-col :span="24"><el-button type="primary">Thêm</el-button></el-col>
-    <el-table  :data="eventTypes">
+    <el-table :data="eventTypes">
       <el-table-column prop="id" label="Id"> </el-table-column>
       <el-table-column prop="code" label="Code"> </el-table-column>
-      <el-table-column prop="description" label="Description"> </el-table-column>
+      <el-table-column prop="description" label="Description">
+      </el-table-column>
       <el-table-column prop="aciton" label="Action">
-        <el-button type="primary" icon="el-icon-edit" circle></el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-edit"
+          circle
+          @click="getEvent_type"
+        ></el-button>
         <el-button type="danger" icon="el-icon-delete" circle></el-button>
       </el-table-column>
     </el-table>
@@ -25,13 +31,17 @@ export default {
     axios
       .get("event-type")
       .then((result) => {
-        
         this.eventTypes = result.data.data;
         console.log(this.eventTypes);
       })
       .catch((err) => {
         console.log(err);
       });
+  },
+  methods: {
+    getEvent_type(id) {
+      console.log(id)
+    },
   },
 };
 </script>
